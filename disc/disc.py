dade1f7ceca4ff07f7e09529605bfac385e22f9e
iehoshia <jepgez@gmail.com>
2017-09-21 15:27:04 -0600

    Agregar nombre
    Agregar nombre

--- disc/disc.py
+++ disc/disc.py
@@ -2,6 +2,7 @@
 # This file is part of the sale_pos module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
+# Josias
 
 import datetime
 from decimal import Decimal
